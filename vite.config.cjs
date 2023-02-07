ce7627d5d9f2f925e0de42b756bf8558ee0aeb11
tong <lbuchiyu@gmail.com>
2023-02-07 23:38:19 +0800

    init: init

---
+++ vite.config.cjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import utools from 'vite-plugin-utools'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        // this is required for the SCSS modules
+        find: /^~(.*)$/,
+        replacement: '$1',
+      },
+    ],
+  },
+  plugins: [
+    react(),
+    utools({
+      external: 'uTools',
+      preload: {
+        path: './src/preload.js',
+        watch: true,
+        name: 'window.preload'
+      },
+      buildUpx: {
+        pluginPath: './plugin.json',
+        outDir: 'upx',
+        outName: '[pluginName]_[version].upx'
+      }
+    })
+  ]
+})
